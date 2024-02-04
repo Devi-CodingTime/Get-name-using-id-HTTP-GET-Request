041eadf3a60adbedfe0b00e6913d14f20ae6ccb9
Devi-CodingTime <devipurnima26899@gmail.com>
2024-02-04 21:50:32 +0530

    kjhjf

--- src/app.js
+++ src/app.js
@@ -6,12 +6,29 @@ const app = express();
 const productNames = JSON.parse(
     fs.readFileSync(`${__dirname}/data/names.json`)
 );
+// console.log(productNames);
 
 //Middlewares
 app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.use('/api/v1/names/:id',(req,res)=>{
+    const {id} = req.params;
+    // console.log(id);
+    const userData = productNames.find(product => id == product.id)
+    if(!userData){
+        return res.status(404).json({
+            status: 'failed',
+            message: "Not found!" 
+        })
+    }
+    return res.status(200).json({
+        status:'success',
+        message:'Product name fetched successfully',
+        data:{userData}
+    });
+})
 
 
 module.exports = app;
