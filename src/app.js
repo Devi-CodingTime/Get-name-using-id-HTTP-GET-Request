93295163335763a2b15c7eee8fec68d1cabe6203
Devi-CodingTime <devipurnima26899@gmail.com>
2024-02-04 22:20:09 +0530

    kjhjf

--- src/app.js
+++ src/app.js
@@ -13,21 +13,49 @@ app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
-app.use('/api/v1/names/:id',(req,res)=>{
-    const {id} = req.params;
-    // console.log(id);
-    const userData = productNames.find(product => id == product.id)
-    if(!userData){
-        return res.status(404).send({
-            status: 'failed',
-            message: "Not found!" 
-        })
+// app.use('/api/v1/names/:id',(req,res)=>{
+//     const {id} = req.params;
+//     // console.log(id);
+//     const userData = productNames.find(product => id == product.id)
+//     if(!userData){
+//         return res.status(404).send({
+//             status: 'failed',
+//             message: "Not found!" 
+//         })
+//     }
+//     return res.status(200).send({
+//         status:'success',
+//         message:'Product name fetched successfully',
+//         data:{userData}
+//     });
+// })
+
+app.get("/api/v1/names/:id", (req, res) => {
+
+    const { id } = req.params;
+
+    const productName = productNames.find((entry) => entry.id == id);
+
+    if (!productName) {
+
+        return res.status(404).send({ status: "failed", message: "Not found!" })
+
     }
+
     return res.status(200).send({
-        status:'success',
-        message:'Product name fetched successfully',
-        data:{userData}
-    });
+
+        status: "success",
+
+        message: "Product name fetched successfully",
+
+        data: {
+
+            productName
+
+        }
+
+    })
+
 })
 
 
